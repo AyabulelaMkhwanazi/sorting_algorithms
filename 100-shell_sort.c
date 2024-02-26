2a38afaea63321a8e06398f62d292e8e4edc25f2
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-02-26 04:29:20 +0200

    modifications made to handle 1 element

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -17,12 +17,8 @@ void shell_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	if (array == NULL || size <= 1)
+	if (array == NULL || size == 0)
 	{
-		if (size == 1)
-		{
-			print_array(array, size);
-		}
 		return;
 	}
 
